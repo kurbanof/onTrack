e51cf7eabc32aee44845b8bf9cbdf753e578d881
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-11-26 22:33:14 +0300

    29.add Button types

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -7,28 +7,33 @@ import BaseButton from '@/components/UI/BaseButton.vue'
 import BaseSelect from '@/components/UI/BaseSelect.vue'
 
 import { isActivityValid } from '@/validators'
-import { PERIOD_SELECT_OPTIONS } from '@/constants'
+import { BUTTON_TYPE_DANGER, PERIOD_SELECT_OPTIONS } from '@/constants'
 
 defineProps({
   activity: {
     required: true,
     type: String,
-    validator: isActivityValid
-
-  }
+    validator: isActivityValid,
+  },
 })
 
 const minutesToComplete = ref(null)
-
 </script>
 <template>
   <li class="flex flex-col gap-2 p-5">
     <div class="flex items-center gap-2 border-4">
-      <BaseButton> <TrashIcon class="h-8" /></BaseButton>
-      <span class="block truncate text-xl">{{ activity }}</span>
+      <BaseButton :type="BUTTON_TYPE_DANGER" class="rounded-none">
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="block truncate text-xl"> {{ activity }}</span>
     </div>
     <div>
-      <BaseSelect class="font-mono" placeholder="hh:mm" :options="PERIOD_SELECT_OPTIONS" v-model="minutesToComplete"/>
+      <BaseSelect
+        class="font-mono"
+        placeholder="hh:mm"
+        :options="PERIOD_SELECT_OPTIONS"
+        v-model="minutesToComplete"
+      />
     </div>
   </li>
 </template>
--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -1,7 +1,24 @@
+<script setup>
+import { BUTTON_TYPE_NEUTRAL, BUTTON_TYPE_DANGER } from '@/constants'
+import {isButtonTypeValid} from '@/validators'
+
+defineProps({
+  type: {
+    type: String,
+    default: BUTTON_TYPE_NEUTRAL,
+    validator: isButtonTypeValid
+  },
+})
+
+const typeClasses = {
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200 ',
+  [BUTTON_TYPE_DANGER]: 'bg-red-300 enabled:hover:bg-red-400 ',
+}
+</script>
 <template>
-   <button
-        class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-      >
-      <slot></slot>
-      </button>
+  <button
+    :class="`${typeClasses[type]} rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
+  >
+    <slot></slot>
+  </button>
 </template>
