44df3579ae99c24852ab652200dcfb655f2ebdda
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-11-29 01:30:40 +0300

    35. create Activity form component

---
+++ src/components/TheActivityForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import { PlusIcon } from '@heroicons/vue/24/outline'
+
+import BaseButton from '@/components/UI/BaseButton.vue'
+import { BUTTON_TYPE_PRIMARY } from '@/constants'
+import { isActivityValid } from '@/validators'
+
+const emit = defineEmits({
+  submit: isActivityValid,
+})
+
+const activity = ref('')
+
+function submit() {
+  emit('submit', activity.value)
+  activity.value = ''
+}
+
+</script>
+<template>
+  <form @submit.prevent="submit" class="sticky bottom-[71px] flex gap-2 border-t-4 bg-white p-5">
+    <input
+      v-model="activity"
+      type="text"
+      placeholder="Activity name"
+      class="w-full gap-2 rounded border-2 px-4 text-xl outline-none hover:bg-purple-100/50 focus:bg-purple-100/50"
+    />
+    <BaseButton :type="BUTTON_TYPE_PRIMARY">
+      <PlusIcon class="h-8" />
+    </BaseButton>
+  </form>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,13 +1,8 @@
 <script setup>
-import { ref } from 'vue'
-
-import { PlusIcon } from '@heroicons/vue/24/outline'
-
 import ActivityItem from '@/components/ActivityItem.vue'
-import BaseButton from '@/components/UI/BaseButton.vue'
+import TheActivityForm from '@/components/TheActivityForm.vue'
 
 import { isActivityValid, validateActivities } from '@/validators'
-import { BUTTON_TYPE_PRIMARY } from '@/constants'
 
 defineProps({
   activities: {
@@ -20,8 +15,6 @@ const emit = defineEmits({
   deleteActivity: isActivityValid,
   createActivity: isActivityValid,
 })
-
-const newActivity = ref('')
 </script>
 <template>
   <div class="flex flex-grow flex-col justify-between">
@@ -33,19 +26,6 @@ const newActivity = ref('')
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <form
-      @submit.prevent="emit('createActivity', newActivity)"
-      class="sticky bottom-[71px] flex gap-2 border-t-4 bg-white p-5"
-    >
-      <input
-        v-model="newActivity"
-        type="text"
-        placeholder="Activity name"
-        class="w-full gap-2 rounded border-2 px-4 text-xl outline-none hover:bg-purple-100/50 focus:bg-purple-100/50"
-      />
-      <BaseButton :type="BUTTON_TYPE_PRIMARY">
-        <PlusIcon class="h-8" />
-      </BaseButton>
-    </form>
+    <TheActivityForm @submit="emit('createActivity', $event)" />
   </div>
 </template>
