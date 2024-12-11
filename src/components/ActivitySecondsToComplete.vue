937d4176613e46b24ad258211f2fc80eb929c033
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-12-11 18:37:25 +0300

    60. update Time of ActivitySecondsToComplete component

--- src/components/ActivitySecondsToComplete.vue
+++ src/components/ActivitySecondsToComplete.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { isActivityValid, validateTimelineItems } from '@/validators';
 import { formatSeconds, getTotalActivitySeconds } from '@/functions';
+import { computed } from 'vue';
 const props = defineProps({
   activity: {
     required: true,
@@ -13,13 +14,16 @@ const props = defineProps({
     validator: validateTimelineItems,
   },
 })
-
-const seconds = formatSeconds(
-  getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete
+const seconds = computed(() => `${sign.value}${formatSeconds(secondsDiff.value)}`)
+const sign = computed(() =>  secondsDiff.value > 0 ? '-' : '+')
+const secondsDiff = computed(() => getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete
 )
+const colorClasses = computed(() => secondsDiff.value > 0 ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600')
+
+const classes = computed(() => `flex items-center bg-purple-100 rounded px-2 text-xl text-purple-600 ${colorClasses.value}`)
 
 </script>
 <template>
-  <div class="flex items-center bg-purple-100 rounded px-2 text-xl text-purple-600">
+  <div :class="classes">
     {{ seconds }}</div>
 </template>
