b56bb4699c1ff97c898408128f089dd39e5a01d9
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-12-18 12:23:28 +0300

    88. refactor Scrolling to current hour

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -3,12 +3,12 @@ import {  watchPostEffect, nextTick } from 'vue';
 import { PAGE_TIMELINE } from '@/constants';
 import { currentPage } from '@/router';
 import TimelineItem from '@/components/TimelineItem.vue'
-import { timelineItems, timelineItemRefs, scrollToHour } from '@/timeline-items';
+import { timelineItems, timelineItemRefs, scrollToHour, scrollToCurrentHour } from '@/timeline-items';
 
 watchPostEffect(async () => {
   if (currentPage.value === PAGE_TIMELINE) {
     await nextTick()
-    scrollToHour(null, false)
+    scrollToCurrentHour(false)
   }
 })
 
--- src/timeline-items.js
+++ src/timeline-items.js
@@ -12,7 +12,7 @@ export function updateTimelineItem(timelineItem, fields) {
 
 export function resetTimelineActivities(activity) {
   timelineItems.value
-    .filter((timelineItem) => hasActivity(timelineItem, activity) )
+    .filter((timelineItem) => hasActivity(timelineItem, activity))
     .forEach((timelineItem) => updateTimelineItem(timelineItem, {
       activityId: null,
       activitySeconds: 0
@@ -24,9 +24,10 @@ export function getTotalActivitySeconds(activity) {
   return timelineItems.value.filter((timelineItem) => hasActivity(timelineItem, activity))
     .reduce((totalSeconds, timelineItem) => Math.round(timelineItem.activitySeconds + totalSeconds), 0)
 }
-
-export function scrollToHour(hour = null, isSmooth = true) {
-  hour ??= currentHour()
+export function scrollToCurrentHour(isSmooth = true) {
+  scrollToHour(currentHour(), isSmooth)
+}
+export function scrollToHour(hour, isSmooth = true) {
   const el = hour === MIDNIGT_HOUR ? document.body : timelineItemRefs.value[hour - 1].$el
   el.scrollIntoView({ behavior: isSmooth ? 'smooth' : 'instant' })
 }
