ba510ff9e6655e87a38f1718b35350e745616ec6
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-12-26 14:46:48 +0300

    111. Dynamically change indicator position

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,29 +1,45 @@
 <script setup>
-import { HUNDRED_PERCENT, SECONDS_IN_DAY, MINUTES_IN_HOUR, SECONDS_IN_MINUTE } from '@/constants';
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
+import { MILLISECONDS_IN_SECONDS, HUNDRED_PERCENT, SECONDS_IN_DAY, MINUTES_IN_HOUR, SECONDS_IN_MINUTE } from '@/constants';
 
+const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 const indicatorRef = ref()
 
-function calculateTopOffset() {
-  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
-}
-function calculateSecondsSinceMidnightInPercentage() {
-  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
-}
+setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECONDS)
+
+const topOffset = computed(
+  () => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT
+)
+
+const secondsSinceMidnightInPercentage = computed(
+  () => (secondsSinceMidnight.value * HUNDRED_PERCENT) / SECONDS_IN_DAY
+)
+
+watchEffect(() => {
+  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
+    secondsSinceMidnight.value = 0
+  }
+})
+
 function calculateSecondsSinceMidnight() {
   const now = new Date()
-  return SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
+
+  return (
+    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
     SECONDS_IN_MINUTE * now.getMinutes() +
     now.getSeconds()
+  )
 }
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
 }
+
+
 </script>
 <template>
   <hr
     ref="indicatorRef"
     class="pointer-events-none absolute z-10 w-full border-b-2 border-green-400/50"
-    :style="{ top: `${calculateTopOffset()}px` }"
+    :style="{ top: `${topOffset}px` }"
   />
 </template>
