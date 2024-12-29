973ab4e20b01aa8f514155fa0d9600444c8bd680
Ahmad Kurbanov <muscleman.ak@gmail.com>
2024-12-29 11:19:49 +0300

    119. add Load app state from local storage

--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,8 @@
 import { APP_NAME } from '@/constants'
 export function load() {
+  const state = localStorage.getItem(APP_NAME)
 
+  return state ? JSON.parse(state) : {}
 }
 export function save(data) {
   localStorage.setItem(APP_NAME, JSON.stringify({
